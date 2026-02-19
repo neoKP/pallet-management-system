1d644aade394e15d956f5bc72325f96a621d8593
nwneosiamlogistics-maker <nw.neosiamlogistics@gmail.com>
2026-02-19 10:31:56 +0700

    fix: อพเดท sw.js cache จาก SVG เปน PNG icons + bump cache version v2

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'ns-pallet-v1';
+const CACHE_NAME = 'ns-pallet-v2';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
   '/index.css',
-  '/icon.svg',
-  '/icon-192.svg',
-  '/icon-512.svg',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png',
   '/manifest.json',
 ];
 
